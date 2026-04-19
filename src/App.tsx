cc6b983b95a190dac79076a22c4a200682e86737
zhoubin2588 <zhoubin2588@users.noreply.github.com>
2026-04-19 14:54:32 +0800

    fix-router-basename

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { HashRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './pages/Home';
@@ -7,7 +7,7 @@ import NotFound from './pages/NotFound';
 
 export default function App() {
   return (
-    <HashRouter>
+    <BrowserRouter basename="/juxin-portfolio">
       <div className="flex min-h-screen flex-col bg-bg">
         <Header />
         <div className="flex-1">
@@ -19,6 +19,6 @@ export default function App() {
         </div>
         <Footer />
       </div>
-    </HashRouter>
+    </BrowserRouter>
   );
 }
